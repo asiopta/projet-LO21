0375f5939f083ac7f416d9ddb74ccbc3eef73bd5
asiopta <128475295+asiopta@users.noreply.github.com>
2024-03-17 11:53:55 +0000

    first class

--- 7_wonnders_duel/game.h
+++ 7_wonnders_duel/game.h
@@ -1,2 +1,25 @@
 #pragma once
+#include <iostream>
+#include <string>
+using namespace std;
 
+class carte{
+private:
+    unsigned int age;
+    unsigned int coutconstruction;
+    //materiauxconstruction;
+
+
+public:
+    void setAge(int newAge) {
+        if (newAge >= 1 && newAge <= 3) {
+            age = newAge;
+        } else {
+            cout<< "Error: Age must be 1, 2, or 3." << endl;
+        }
+    }
+
+    int getAge() const {
+        return age;
+    }
+};
