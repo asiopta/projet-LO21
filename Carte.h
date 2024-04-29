7743538f25d260735992e209907e23064f6745d2
KolinMTG <colin.manyri@gmail.com>
2024-04-29 20:26:54 +0200

    ajout de JetonScience, et des fonctions associées sauf exec_capacite_scice

--- Carte.h
+++ Carte.h
@@ -74,37 +74,6 @@ enum class Capacite{
 };
 
 
-
-enum class CapaciteScience{
-    agriculture,
-    architecture,
-    economie,
-    loi,
-    maconnerie,
-    urbanisme,
-    theologie,
-    strategie,
-    philosophie,
-    mathematique
-};
-
-std::ostream& operator<<(std::ostream& f, const CapaciteScience& capacite) {
-    switch (capacite) {
-        case CapaciteScience::agriculture: f << "Agriculture"; break;
-        case CapaciteScience::architecture: f << "Architecture"; break;
-        case CapaciteScience::economie: f << "Économie"; break;
-        case CapaciteScience::loi: f << "Loi"; break;
-        case CapaciteScience::maconnerie: f << "Maçonnerie"; break;
-        case CapaciteScience::urbanisme: f << "Urbanisme"; break;
-        case CapaciteScience::theologie: f << "Théologie"; break;
-        case CapaciteScience::strategie: f << "Stratégie"; break;
-        case CapaciteScience::philosophie: f << "Philosophie"; break;
-        case CapaciteScience::mathematique: f << "Mathématique"; break;
-        default:
-            f.setstate(std::ios_base::failbit);
-    }
-    return f;
-}
 class Carte{
 protected:
     unsigned int cout_construction;
---
+++ Plateau.cpp
@@ -0,0 +1,60 @@
+#include Plateau.h
+
+JetonScience::JetonScience(CapaciteScience capacite) : capacite(capacite) {}
+void JetonScience::exec_capacite_science() {
+    switch (capacite) {
+        exec_agriculture(); break;
+        case CapaciteScience::architecture: exec_architecture(); break;
+        case CapaciteScience::economie: exec_economie(); break;
+        case CapaciteScience::loi: exec_loi(); break;
+        case CapaciteScience::maconnerie: exec_maconnerie(); break;
+        case CapaciteScience::urbanisme: exec_urbanisme(); break;
+        case CapaciteScience::theologie: exec_theologie(); break;
+        case CapaciteScience::strategie: exec_strategie(); break;
+        case CapaciteScience::philosophie: exec_philosophie(); break;
+        case CapaciteScience::mathematique: exec_mathematique(); break;
+        default:
+            std::cout << "Erreur";
+    }
+}
+
+void exec_agriculture() {
+    // implementation of exec_agriculture
+}
+
+void exec_architecture() {
+    // implementation of exec_architecture
+}
+
+void exec_economie() {
+    // implementation of exec_economie
+}
+
+void exec_loi() {
+    // implementation of exec_loi
+}
+
+void exec_maconnerie() {
+    // implementation of exec_maconnerie
+}
+
+void exec_urbanisme() {
+    // implementation of exec_urbanisme
+}
+
+void exec_theologie() {
+    // implementation of exec_theologie
+}
+
+void exec_strategie() {
+    // implementation of exec_strategie
+}
+
+void exec_philosophie() {
+    // implementation of exec_philosophie
+}
+
+void exec_mathematique() {
+    // implementation of exec_mathematique
+}
+
