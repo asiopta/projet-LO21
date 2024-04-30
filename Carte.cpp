809b46020c274996bd4ffb69f9057c5317d3d55c
KolinMTG <colin.manyri@gmail.com>
2024-04-30 12:50:10 +0200

    Rien d'important

--- Carte.cpp
+++ Carte.cpp
@@ -94,23 +94,6 @@ std::ostream& operator<<(std::ostream& f, const Capacite& capacite) {
     return f;
 }
 
-std::ostream& operator<<(std::ostream& f, const CapaciteScience& capacite) {
-    switch (capacite) {
-        case CapaciteScience::agriculture: f << "Agriculture"; break;
-        case CapaciteScience::architecture: f << "Architecture"; break;
-        case CapaciteScience::economie: f << "Économie"; break;
-        case CapaciteScience::loi: f << "Loi"; break;
-        case CapaciteScience::maconnerie: f << "Maçonnerie"; break;
-        case CapaciteScience::urbanisme: f << "Urbanisme"; break;
-        case CapaciteScience::theologie: f << "Théologie"; break;
-        case CapaciteScience::strategie: f << "Stratégie"; break;
-        case CapaciteScience::philosophie: f << "Philosophie"; break;
-        case CapaciteScience::mathematique: f << "Mathématique"; break;
-        default:
-            f.setstate(std::ios_base::failbit);
-    }
-    return f;
-}
 
 
 
