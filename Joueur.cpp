9da9ba0daa372a919e5683ca2f9c004bf317b130
asiopta <128475295+asiopta@users.noreply.github.com>
2024-05-01 15:49:12 +0000

    AMEN: j'ai pas mal avancé sur la classe Joueur et ses méthodes

--- Joueur.cpp
+++ Joueur.cpp
@@ -1,4 +1,7 @@
 #include "Joueur.h"
+#include <string.h>
+#include <string>
+
 
 /*------------------classe Joueur----------------------*/
 //constructeur
@@ -15,8 +18,45 @@ Joueur::~Joueur(){
     for(int i=0; i< get_nb_jetons_science(); i++) free(jetons_science[i]);
 }
 
-//les getteurs
-unsigned int Joueur::get_quantite_ressource_primaire(const RessourcePrimaire& symbole) const{
+// _____les getteurs_____//
+
+//getteur de Nb de Cartes / jetons
+unsigned int Joueur::getNbCartesConstruites(){
+    unsigned int i = 0;
+    while(i<60 && cartes_construite[i]!= NULL){
+        i++;
+    }
+    return i;
+}
+
+unsigned int Joueur::getNbMerveillesConstruites(){
+    unsigned int i = 0;
+    while(i<4 && merveille_construite[i]!= NULL){
+        i++;
+    }
+    return i;
+}
+
+unsigned int Joueur::getNbJetonsScience(){
+    unsigned int i = 0;
+    while(i<6 && jetons_science[i]!= NULL){
+        i++;
+    }
+    return i;
+}
+
+
+
+unsigned int Joueur::getNbSymbolesScience(){
+    unsigned int i = 0;
+    while(i<6 && SymboleScience[i]!= NULL){
+        i++;
+    }
+    return i;
+}
+
+//les getteurs de ressources
+unsigned int Joueur::getQuantiteDeRessourcePrimaire(const RessourcePrimaire& symbole){
         Carte* constr = *cartes_construite;
         unsigned int res=0;
         for(int i=0; i<60; i++){
@@ -41,3 +81,24 @@ unsigned int Joueur::get_quantite_ressource_secondaire(const RessourceSecondaire
         }
         return res;
     };
+
+
+//d'autres méthodes utiles
+void Joueur::gagnerArgent(unsigned int argent){
+    unsigned int res = this->getMonnaie();
+    res += argent;
+    this->setMonnaie(res);
+}
+
+void Joueur::gagnerPtVictoire(unsigned int p){
+    unsigned int res = this->getPtVictoire();
+    res += p;
+    this->setPtVictoire(res);
+}
+
+//méthodes de vérification
+bool Joueur::estConstructible(const Carte& carte, const PlateauCartes& p){
+    if(this->getCout(carte) < this->getMonnaie()) return true;
+    return false;
+
+}
