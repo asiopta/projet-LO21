0622b5c07db3a00afe4ca9e2d9dc282d2195c4aa
asiopta <nticha.asiopta1@gmail.com>
2024-06-11 08:09:38 +0200

    ignore

--- main.cpp
+++ main.cpp
@@ -64,12 +64,15 @@ int SevenWondersDuel(Controleur& controleur, Joueur* joueur_actif){ //le 1er jou
             Joueur* joueur_gagnant = controleur.determineGagnant();
             if (joueur_gagnant == controleur.getJoueur1()){
                 std::cout << "Le joueur 1 a gagné!" << endl;
+                return 1;
             }
             else if (joueur_gagnant == controleur.getJoueur2()){
                 std::cout << "Le joueur 2 a gagné!" << endl;
+                return 2;
             }
             else {
                 std::cout << "Egalité!" << endl;
+                return 0;
             }
         }
     }
